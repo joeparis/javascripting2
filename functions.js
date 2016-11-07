bd2a1473c01914dde9fee84861877555a019a20b
MKH103-01 <MKH103-01@lbcc.linnbenton.edu>
2016-11-07 11:03:55 -0800

    Modified functions

--- functions.js
+++ functions.js
@@ -1,5 +1,9 @@
 var eat = function(food) {
     return food + " tasted really good.";
-}
+};
+
+// function eat(food) {
+//     return food + " tasted really good.";
+// }
 
 console.log(eat("bananas"));
